d4a1bd8bfdcc1fba9b758cba954c91e5b0bd91ae
Adarsh <adarshgupta5002@gmail.com>
2026-04-12 19:39:54 +0530

    Adding all the backend files

---
+++ nimbusx-backend/src/main/java/com/nimbusx/backend/NimbusxBackendApplication.java
@@ -0,0 +1,13 @@
+ package com.nimbusx.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NimbusxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NimbusxBackendApplication.class, args);
+	}
+
+}
---
+++ nimbusx-backend/src/test/java/com/nimbusx/backend/NimbusxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nimbusx.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NimbusxBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
